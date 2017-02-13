99463093d4c7578c222d420c3a786375de9f156f
nbf <biaofei@staff.sina.com.cn>
2017-02-13 14:44:28 +0800

    spark: add mysqlSpark

---
+++ service-spark/src/main/java/com/mzjf/MysqlSpark.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2014-2015 XXX, Inc. All Rights Reserved.
+ */
+
+package com.mzjf;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.DataFrame;
+import org.apache.spark.sql.SQLContext;
+
+public final class MysqlSpark {
+
+    public static void main(String[] args) throws Exception {
+
+        JavaSparkContext ctx = new JavaSparkContext(new SparkConf().setMaster("local").setAppName(
+                "MysqlSpark"));
+        SQLContext sqlContext = new SQLContext(ctx);
+
+        String[] predicates = new String[] { "user_roulette_log_id <= 10000",
+                "user_roulette_log_id > 10000 and user_roulette_log_id <= 20000" };
+        String url = "jdbc:mysql://localhost:3306/sparkdb?user=root&password=123456";
+
+        DataFrame dataFrame = sqlContext.jdbc(url, "user_roulette_log", predicates);
+        System.err.println(dataFrame.count());
+        System.err.println(dataFrame.filter("roulette_status = 1").count());
+        Thread.sleep(600000);
+        ctx.stop();
+    }
+}
