f4d0b1bfba642a7d0e514eda150162a693d577db
nbf <biaofei@staff.sina.com.cn>
2017-03-13 17:12:59 +0800

    algotithm: add InnerSort class

--- service-algotithm/src/main/java/com/nbf/inner/sort/InnerSort.java
+++ service-algotithm/src/main/java/com/nbf/inner/sort/InnerSort.java
@@ -5,8 +5,8 @@
 package com.nbf.inner.sort;
 
 import java.util.Arrays;
-
-import bsh.This;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
 * @author nbf
@@ -19,6 +19,21 @@ import bsh.This;
 */
 
 public class InnerSort {
+    public List<String> letterCombinations(String digits) {
+        LinkedList<String> ans = new LinkedList<String>();
+        String[] mapping = new String[] { "0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs",
+                "tuv", "wxyz" };
+        ans.add("");
+        for (int i = 0; i < digits.length(); i++) {
+            int x = Character.getNumericValue(digits.charAt(i));
+            while (ans.peek().length() == i) {
+                String t = ans.remove();
+                for (char s : mapping[x].toCharArray())
+                    ans.add(t + s);
+            }
+        }
+        return ans;
+    }
 
     /**
      * 总结：
@@ -50,7 +65,8 @@ public class InnerSort {
         //        innerSort.directChioceSort();
         //        innerSort.headSort();
         //        innerSort.bubblingSort();
-        innerSort.quickSort();
+//        innerSort.quickSort();
+        innerSort.letterCombinations("23");
     }
 
     /**
