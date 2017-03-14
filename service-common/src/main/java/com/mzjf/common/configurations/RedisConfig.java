e0b44d188fc96264c12a33ac791c4b3891034751
nbfcome <nbfcome@163.com>
2017-03-14 20:30:46 +0800

    temp: update resources

--- service-common/src/main/java/com/mzjf/common/configurations/RedisConfig.java
+++ service-common/src/main/java/com/mzjf/common/configurations/RedisConfig.java
@@ -4,31 +4,31 @@
 
 package com.mzjf.common.configurations;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.data.redis.connection.RedisSentinelConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Profile;
+//import org.springframework.data.redis.connection.RedisSentinelConfiguration;
+//import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+//
+//import com.mzjf.common.consts.CustomProfiles;
 
-import com.mzjf.common.consts.CustomProfiles;
-
-@Configuration
+//@Configuration
 public class RedisConfig {
 
-    @Bean
-    @Profile(CustomProfiles.TEST)
-    public JedisConnectionFactory jedisConnectionFactory() {
-        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
-        return jedisConnectionFactory;
-    }
+//    @Bean
+//    @Profile(CustomProfiles.TEST)
+//    public JedisConnectionFactory jedisConnectionFactory() {
+//        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
+//        return jedisConnectionFactory;
+//    }
 
-    @Bean
-    @Profile(CustomProfiles.CLOUD)
-    public JedisConnectionFactory jedisSentinelConnection() {
-        RedisSentinelConfiguration sentinelConfig = new RedisSentinelConfiguration()
-                .master("mymaster").sentinel("127.0.0.1", 26379).sentinel("127.0.0.1", 26380);
-        return new JedisConnectionFactory(sentinelConfig);
-    }
+//    @Bean
+//    @Profile(CustomProfiles.CLOUD)
+//    public JedisConnectionFactory jedisSentinelConnection() {
+//        RedisSentinelConfiguration sentinelConfig = new RedisSentinelConfiguration()
+//                .master("mymaster").sentinel("127.0.0.1", 26379).sentinel("127.0.0.1", 26380);
+//        return new JedisConnectionFactory(sentinelConfig);
+//    }
 
     //    @Bean
     //    public RedisTemplate redisTemplate(RedisConnectionFactory factory) {
